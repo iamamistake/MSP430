10a75cb5c7cb01011ad9ac48ca0578d15fdf71b7
Shivam Khamble <73642663+iamamistake@users.noreply.github.com>
2022-02-17 23:24:27 +0530

    Update serial_i2c_acc.c

--- serial_i2c_acc.c
+++ serial_i2c_acc.c
@@ -1,27 +1,68 @@
 #include <msp430.h>
-#define PUSH_BUTTON BIT3
-//char received_data [3];
+
+// LIS3DH definitions (W25Q80DVSNIG)
+#define STATUS_REG_AUX 0x07
+#define OUT_ADC1_L 0x08
+#define OUT_ADC1_H 0x09
+#define OUT_ADC2_L 0x0A
+#define OUT_ADC2_H 0x0B
+#define OUT_ADC3_L 0x0C
+#define OUT_ADC3_H 0x0D
+#define WHO_AM_I 0x0F
+#define CTRL_REG0 0x1E
+#define TEMP_CFG_REG 0x1F
+#define CTRL_REG1 0x20
+#define CTRL_REG2 0x21
+#define CTRL_REG3 0x22
+#define CTRL_REG4 0x23
+#define CTRL_REG5 0x24
+#define CTRL_REG6 0x25
+#define REFERENCE 0x26
+#define STATUS_REG 0x27
+#define OUT_X_L 0x28
+#define OUT_X_H 0x29
+#define OUT_Y_L 0x2A
+#define OUT_Y_H 0x2B
+#define OUT_Z_L 0x2C
+#define OUT_Z_H 0x2D
+#define FIFO_CTRL_REG 0x2E
+#define FIFO_SRC_REG 0x2F
+#define INT1_CFG 0x30
+#define INT1_SRC 0x31
+#define INT1_THS 0x32
+#define INT1_DURATION 0x33
+#define INT2_CFG 0x34
+#define INT2_SRC 0x35
+#define INT2_THS 0x36
+#define INT2_DURATION 0x37
+#define CLICK_CFG 0x38
+#define CLICK_SRC 0x39
+#define CLICK_THS 0x3A
+#define TIME_LIMIT 0x3B
+#define TIME_LATENCY 0x3C
+#define TIME_WINDOW 0x3D
+#define ACT_THIS 0x3E
+#define ACT_DUR 0x3F
+#define READ BIT7
+
+// Pin definitions
+#define SCLK BIT6
+#define SDA BIT7
+
 char received_data;
-unsigned int i = 0;
 
 void main (void) {
-     WDTCTL = WDTPW | WDTHOLD; // disabling the watchdog timer
+    WDTCTL = WDTPW | WDTHOLD; // disabling the watchdog timer
 
     BCSCTL1 = CALBC1_1MHZ;
     DCOCTL = CALDCO_1MHZ; // setting the clock rate to 1MHz
 
-    P1IE |= PUSH_BUTTON;
-    P1IES |= PUSH_BUTTON;
-    P1IFG &= ~PUSH_BUTTON;
-    _enable_interrupts();
-
-
-    P1SEL = BIT6 | BIT7;
-    P1SEL2 = BIT6 | BIT7;
+    P1SEL = SCLK | SDA;
+    P1SEL2 = SCLK | SDA;
     UCB0CTL1 |= UCSWRST + UCSSEL_2;
     UCB0CTL0 |= UCMST + UCMODE_3 + UCSYNC;
     UCB0BR0 = 10;
-    UCB0I2CSA =  0x51; // Slave address (without R/W bit)
+    UCB0I2CSA =  0x18; // Slave address (without R/W bit)
     UCB0CTL1 &= ~UCSWRST;
 
     IE2 |= UCB0TXIE;
@@ -44,20 +85,6 @@ void main (void) {
     }
 }
 
-//#pragma vector=PORT1_VECTOR
-//__interrupt void PORT1_ISR (void) {
-//    UCB0CTL1 |= UCTR;
-//    UCB0CTL1 |= UCTXSTT;
-//    while (UCB0CTL1 & UCTXSTP);
-//
-//
-//
-//    UCB0CTL1 &= ~UCTR;
-//    UCB0CTL1 |= UCTXSTT;
-//
-//    P1IFG &= ~PUSH_BUTTON;
-//}
-
 #pragma vector=USCIAB0TX_VECTOR
 __interrupt void USCIAB0TX_ISR (void) {
 
@@ -68,22 +95,9 @@ __interrupt void USCIAB0TX_ISR (void) {
     }
 
     else {
-        UCB0TXBUF = 0x01;
+        UCB0TXBUF = WHO_AM_I;
 //        IFG2 &= ~ UCB0TXIFG;
     }
     IFG2 &= ~ UCB0TXIFG;
     IFG2 &= ~ UCB0RXIFG;
 }
-
-
-//#pragma vector=USCIAB0RX_VECTOR
-//__interrupt void USCIAB0RX_ISR (void) {
-////    received_data [i] = UCB0RXBUF;
-////    if (sizeof(received_data)==3) {
-////        i=0;
-////        UCB0CTL1 |= UCTXSTP;
-////    }
-//    received_data = UCB0RXBUF;
-//
-//    IFG2 &= ~ UCB0RXIFG;
-//}
